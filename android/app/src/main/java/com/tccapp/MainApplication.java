76834d8a948be32ac42d0681aec45de8eb0b23bc
Gerson <gersoncarneiro@msn.com>
2020-07-07 03:11:44 -0300

    unimodules

--- android/app/src/main/java/com/tccapp/MainApplication.java
+++ android/app/src/main/java/com/tccapp/MainApplication.java
@@ -1,7 +1,11 @@
-package com.tccapp;
 
+package com.tccapp;
+import com.tccapp.generated.BasePackageList;
 import android.app.Application;
 import android.content.Context;
+import org.unimodules.adapters.react.ModuleRegistryAdapter;
+import org.unimodules.adapters.react.ReactModuleRegistryProvider;
+import org.unimodules.core.interfaces.SingletonModule;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.guhungry.rnphotomanipulator.RNPhotoManipulatorPackage;
@@ -11,12 +15,16 @@ import com.reactnativecommunity.webview.RNCWebViewPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
+
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(new BasePackageList().getPackageList(), null);
 
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
@@ -31,6 +39,13 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+
+          // Add unimodules
+          List<ReactPackage> unimodules = Arrays.<ReactPackage>asList(
+            new ModuleRegistryAdapter(mModuleRegistryProvider)
+          );
+          packages.addAll(unimodules);
+
           return packages;
         }
 
